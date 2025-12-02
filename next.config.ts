7291a2385fbab59ab8183825dd40bd84c3e37188
Abdulla Alshameeri <abdulla.alshameeri@example.com>
2025-12-03 02:18:23 +0300

    Fix firebase-admin resolution for Vercel

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,15 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  experimental: {
+    turbo: {
+      resolveAlias: {
+        // إصلاح firebase-admin داخل Vercel
+        "firebase-admin": require.resolve("firebase-admin"),
+      },
+    },
+  },
 };
 
 export default nextConfig;
+// https://github.com/firebase/firebase-admin-node/issues/897#issuecomment-898879744
