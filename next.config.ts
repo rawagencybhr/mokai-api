67143d5eb73e73c7f8368dc84c5fada681e15d53
Abdulla Alshameeri <abdulla.alshameeri@example.com>
2025-12-03 02:25:51 +0300

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,11 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  // إيقاف Turbopack وإجبار Next.js على استخدام Webpack
+  turbopack: false,
+
   webpack: (config) => {
+    // إصلاح firebase-admin في Vercel
     config.resolve.alias["firebase-admin"] = require.resolve("firebase-admin");
     return config;
   },
