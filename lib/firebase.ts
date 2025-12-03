198f6717adf6b3c224f616682c0fe07781ef12ee
rawagencybhr <rawagency.bhr@gmail.com>
2025-12-03 04:55:57 +0300

    Update firebase.ts

--- lib/firebase.ts
+++ lib/firebase.ts
@@ -1,13 +1,22 @@
-import admin from "firebase-admin";
+import * as admin from "firebase-admin";
+
+let app;
 
 if (!admin.apps.length) {
-  admin.initializeApp({
-    credential: admin.credential.cert({
-      project_id: process.env.FIREBASE_PROJECT_ID,
-      client_email: process.env.FIREBASE_CLIENT_EMAIL,
-      private_key: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, "\n"),
-    } as any),
+  const base64 = process.env.FIREBASE_SERVICE_ACCOUNT_BASE64;
+
+  if (!base64) {
+    throw new Error("Missing FIREBASE_SERVICE_ACCOUNT_BASE64 environment variable");
+  }
+
+  const jsonString = Buffer.from(base64, "base64").toString("utf8");
+  const serviceAccount = JSON.parse(jsonString);
+
+  app = admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount),
   });
+} else {
+  app = admin.app();
 }
 
-export default admin;
+export default app;
