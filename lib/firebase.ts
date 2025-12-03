08e9fd27ec63dc0d3fbd7ecd82bf6bc97ab25cf9
rawagencybhr <rawagency.bhr@gmail.com>
2025-12-03 05:56:12 +0300

    Update firebase.ts

--- lib/firebase.ts
+++ lib/firebase.ts
@@ -1,19 +1,25 @@
-import admin, { ServiceAccount } from "firebase-admin";
+import * as admin from "firebase-admin";
 
-let app;
+let app: admin.app.App;
 
+// منع إعادة التهيئة في Vercel أو أثناء Hot Reload
 if (!admin.apps.length) {
   const base64 = process.env.FIREBASE_SERVICE_ACCOUNT_BASE64;
-  if (!base64) throw new Error("Missing FIREBASE_SERVICE_ACCOUNT_BASE64");
+
+  if (!base64) {
+    throw new Error("Missing FIREBASE_SERVICE_ACCOUNT_BASE64");
+  }
 
   const jsonString = Buffer.from(base64, "base64").toString("utf8");
-  const serviceAccount = JSON.parse(jsonString) as ServiceAccount;
+  const serviceAccount = JSON.parse(jsonString);
 
   app = admin.initializeApp({
     credential: admin.credential.cert(serviceAccount),
   });
+
 } else {
   app = admin.app();
 }
 
 export default app;
+export { admin };
